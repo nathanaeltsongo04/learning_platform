30c0c099e54c192d38fe14be3ccfb53861a74d29
Jean-Louis11 <jeanlouisdouglas93@gmail.com>
2024-08-29 08:12:58 +0300

    some updates

--- learning_platform/settings.py
+++ learning_platform/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yqccmbzz&$ojuu(#_axkgf!c1uukvqe5ouocjtx5$&@#)!m8%i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
